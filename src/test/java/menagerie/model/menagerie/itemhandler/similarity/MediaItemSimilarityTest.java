5d41dfcbe450d0ea95bebefcb4d0af92edcad98e
Elias <elias.santner@edu.htl-villach.at>
2023-01-05 21:57:55 +0100

    try to fix test exec in pipeline

--- src/test/java/menagerie/model/menagerie/itemhandler/similarity/MediaItemSimilarityTest.java
+++ src/test/java/menagerie/model/menagerie/itemhandler/similarity/MediaItemSimilarityTest.java
@@ -2,6 +2,7 @@ package menagerie.model.menagerie.itemhandler.similarity;
 
 import menagerie.model.menagerie.GroupItem;
 import menagerie.model.menagerie.MediaItem;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.testfx.framework.junit5.ApplicationExtension;
@@ -27,6 +28,21 @@ public class MediaItemSimilarityTest {
     m.initializeHistogram();
     m.initializeMD5();
     return m;
+  }
+
+  @BeforeAll
+  static void setupHeadless() throws Exception {
+        /* Set "headless" property to true to enable headless testing (using -Dheadless=true)
+           PLEASE NOTE:
+           Headless tests must be run at a MAX resolution of 1280x800.
+           Higher resolutions cause buffer overflows */
+    if (Boolean.getBoolean("headless")) {
+      System.setProperty("testfx.robot", "glass");
+      System.setProperty("testfx.headless", "true");
+      System.setProperty("prism.order", "sw");
+      System.setProperty("prism.text", "t2k");
+      System.setProperty("java.awt.headless", "true");
+    }
   }
 
   @Test
